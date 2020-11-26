632653464d24f86f9fa25ba3687537ea9a7fa6e0
dutchi13 <59352297+dutchi13@users.noreply.github.com>
2020-11-26 10:30:42 +0100

    Create spr_06.cpp

---
+++ Sprawozdania/spr_06.cpp
@@ -0,0 +1,49 @@
+
+/*------------------------------------------------------------
+
+Temat projektu: Gra platformowa 
+
+Zakres projektu:
+    1.	Tworzenie okna z tytulem i o odpowiedniej wielkosci
+	2.	Wyswietlanie obiektu gracza (bez tekstury)
+	3.	Wstepne poruszanie graczem (bez grawitacji, w tej chwili nie dziala to tak jak w planie)
+	4.	klasa Menu (choc wciaz jest nie dokonczona)
+	5.	klasa Level obejmujaca textury poziomu (poziomy skladane z plytek o roznych texturach i wlasciwosciach)
+
+Co zrealizowalem w biezacym tygodniu?
+
+	Powstala klasa Level generujaca poziom 
+   
+Co planuje na kolejny tydzien?
+	
+	Wykrywanie zderzen.
+	Kamera podazajaca za graczem.
+	
+	
+Kod źródłowy gry opublikowany jest na stronie github.com pod adresem https://github.com/davy52/SFML-Game
+	zmiany z ostatnich 2 tygodni w galezi devLevelClass
+------------------------------------------------------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
