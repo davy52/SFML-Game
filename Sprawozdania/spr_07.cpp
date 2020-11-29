1b1b6574d6b628947789e4794e444359d6e838d0
dutchi13 <59352297+dutchi13@users.noreply.github.com>
2020-11-29 23:51:28 +0100

    gitignore update

---
+++ Sprawozdania/spr_07.cpp
@@ -0,0 +1,48 @@
+
+/*------------------------------------------------------------
+
+Temat projektu: Gra platformowa 
+
+Zakres projektu:
+    1.	Tworzenie okna z tytulem i o odpowiedniej wielkosci
+	2.	Wyswietlanie obiektu gracza (bez tekstury)
+	3.	Wstepne poruszanie graczem (bez grawitacji, w tej chwili nie dziala to tak jak w planie)
+	4.	klasa Menu (choc wciaz jest nie dokonczona)
+	5.	klasa Level obejmujaca textury poziomu (poziomy skladane z plytek o roznych texturach i wlasciwosciach)
+    6.  Minimapa i widok podazajacy 
+
+Co zrealizowalem w biezacym tygodniu?
+
+	Powstala minimapa i widok podazajacy za graczem
+   
+Co planuje na kolejny tydzien?
+	
+	Wykrywanie zderzen.
+	Estetyka gry i piersze poziomy 
+	
+Kod źródłowy gry opublikowany jest na stronie github.com pod adresem https://github.com/davy52/SFML-Game
+------------------------------------------------------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
